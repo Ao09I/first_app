d1742c37c79c196110bc16d9d89955e5a374738a
Ao09I <skmaoi.000@icloud.com>
2022-11-02 00:44:41 +0900

    complete fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     if @post.save
     redirect_to("/posts/index")
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
 
   end
