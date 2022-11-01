aafd2640ea6d885402ede617c851fd197f21e614
Ao09I <skmaoi.000@icloud.com>
2022-11-01 10:53:53 +0900

    Nov_1th commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show"
   get '/' => "home#top"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
